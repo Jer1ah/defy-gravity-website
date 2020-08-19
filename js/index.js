3c6110ebdc628d117f700b1a164c555ffa0aa26f
Jer1ah <jeriah97bowers@yahoo.com>
2020-08-18 21:48:58 -0400

    styling slick slider

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    $('.slider').slick();
+    $('.slider').slick({
+        prevArrow: '<div class="prevButton"><img src="../images/back.svg"/></div>',
+        nextArrow: '<div class="nextButton"><img src="../images/next.svg"/></div>'
+    });
 });
