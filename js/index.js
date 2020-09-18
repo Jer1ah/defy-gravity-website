d7ce750f3158f6b7f696888b105c75e3f43efa19
Jer1ah <jeriah97bowers@yahoo.com>
2020-09-17 20:16:19 -0400

    making tickets section responsive

--- js/index.js
+++ js/index.js
@@ -19,4 +19,16 @@ const siteLogoController = (function() {
             _logo.parentNode.style.left = "1rem";
         }
     }
+
+    window.onresize = () => {
+        if(window.innerWidth < 1376) {
+            _logo.style.height = "6rem";
+            _logo.parentNode.style.top = ".75rem";
+            _logo.parentNode.style.left = "3.3rem";
+        } else {
+            _logo.style.height = "11rem";
+            _logo.parentNode.style.top = "1.5rem";
+            _logo.parentNode.style.left = "1rem";
+        }
+    }
 }());
